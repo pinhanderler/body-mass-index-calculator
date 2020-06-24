8fb52b25f153aa270f56c73b7442c63038f6f800
pinhanderler <61183129+pinhanderler@users.noreply.github.com>
2020-06-24 15:25:17 +0300

    Update DatabaseHelper.java

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -23,9 +23,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_USER_EMAIL = "email";
     private static final String COLUMN_USER_BIRTHDAY_DAY = "birthday_date";
     private static final String COLUMN_USER_PASSWORD = "password";
+	private static final String COLUMN_USER_BIRTHDAY_DATE = null;
 
     private String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + "("
-            + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER NAME + " TEXT,"
+            + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + " TEXT,"
             + COLUMN_USER_NAME + " TEXT," + COLUMN_USER_SURNAME + " TEXT," + COLUMN_USER_EMAIL + " TEXT,"
             + COLUMN_USER_BIRTHDAY_DATE + " TEXT," + COLUMN_USER_PASSWORD + " TEXT" + ")";
 
@@ -38,139 +39,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db){ db.execSQL(CREATE_USER_TABLE);}
 
     public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
+
         db.execSQL(DROP_USER_TABLE);
+
         onCreate(db);
-    }
-
-    public void addUser(User user) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_USER_NAME, user.getUserName());
-        values.put(COLUMN_USER_NAME, user.getName());
-        values.put(COLUMN_USER_Surname, user.getSurname());
-        values.put(COLUMN_USER_EMAIL, user.getEmail());
-        values.put(COLUMN_USER_BIRTHDAY_DATE, user.getBirthdayDate());
-        values.put(COLUMN_USER_PASSWORD, user.getPassword());
-
-        db.insert(TABLE_USER, null, values);
-        db.close();
-    }
-
-    public List<User> getAllUser(){
-
-        String[] columns = {
-                COLUMN_USER_ID,
-                COLUMN_USER_USER_NAME,
-                COLUMN_USER_NAME,
-                COLUMN_USER_SURNAME,
-                COLUMN_USER_EMAIL,
-                COLUMN_USER_BIRTHDAY_DATE,
-                COLUMN_USER_PASSWORD,
-        };
-
-        Dize sortOrder =  COLUMN_USER_NAME  +  " ASC " ;    
-                 
-        List<User> userList = new ArrayList<User>();
-
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        Cursor cursor = db.query(TABLE_USER,
-                columns,
-                null,
-                null,
-                null,
-                null,
-                sortOrder);
-
-        if( cursor.moveToFirst()){
-            do {
-                User user = new User();
-                user.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(COLUMN_USER_ID))));
-                user.setUserId((cursor.getString(cursor.getColumnIndex(COLUMN_USER_ID))));
-                user.setName((cursor.getString(cursor.getColumnIndex(COLUMN_USER_NAME))));
-                user.setSurname((cursor.getString(cursor.getColumnIndex(COLUMN_USER_SURNAME))));
-                user.setEmail((cursor.getString(cursor.getColumnIndex(COLUMN_USER_EMAIL))));
-                user.setBirthdayDate((cursor.getString(cursor.getColumnIndex(COLUMN_USER_BIRTHDAY_DATE))));
-                user.setPassword((cursor.getString(cursor.getColumnIndex(COLUMN_USER_PASSWORD))));
-
-                userList.add(user);
-            } while (cursor.moveToNext()); 
-        }
-        cursor.close();
-        db.close();
-
-        return userList;
-    }
-
-    public void updateUser(User user){
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_USER_USER_NAME, user.getUserName());
-        values.put(COLUMN_USER_NAME, user.getName());
-        values.put(COLUMN_USER_SURNAME, user.getSurname());
-        values.put(COLUMN_USER_EMAIL, user.getEmail());
-        values.put(COLUMN_USER_BIRTHDAY_DATE, user.getBirthdayDate());
-        values.put(COLUMN_USER_PASSWORD, user.getPassword());
-
-        db.update(TABLE_USER, values,COLUMN_USER_ID + " = ?", new String[]{String.valueOf(user.getId())});
-
-        db.close();
-    }
-
-    public void deleteUser(User user) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        db.delete(TABLE_USER, COLUMN_USER_ID + " =?", new String[]{String.valueOf(user.getId())});
-        db.close();
-    }
-
-    public boolean checkUser(String email) {
-
-        String[] columns = {
-                COLUMN_USER_ID
-        };
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        String selection = COLUMN_USER_USER_NAME + " = ?";
-
-        String[] selectionArgs = {email};
-
-        Cursor cursor = db.query(TABLE_USER,
-                columns,
-                selection,
-                selectionArgs,
-                null,
-                null,
-                null);
-        int cursorCount = cursor.getCount();
-        cursor.close();
-        db.close();
-        
-        return cursorCount > 0;
-  
-    public boolean checkUser(String username, String password){
-
-        public boolean checkUser(String username, String password) {
-        String[] columns = { COLUMN_USER_ID };
-        SQLiteDatabase db = this.getReadableDatabase();
-        String selection = COLUMN_USER_USER_NAME + " = ?" + " AND " + COLUMN_USER_PASSWORD + " = ?";
-        String[] selectionArgs = { username, password };
-
-        Cursor cursor = db.query(TABLE_USER,
-                columns,
-                selection,
-                selectionArgs,
-                null,
-                null,
-                null);
-
-        int cursorCount = cursor.getCount();
-
-        cursor.close();
-        db.close();
-
-         return cursorCount > 0;
     }
 }
